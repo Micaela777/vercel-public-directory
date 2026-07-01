08032b82ed3bd85ec9796c370b73ee87f211bd33
Micaela Medina <medimica@gmail.com>
2026-07-01 20:26:15 -0300

    se mueve a next

---
+++ pages/api/index.js
@@ -0,0 +1,12 @@
+export default function mica(req, res){
+    res.status(200).json({
+        body: req.body,
+        query: req.query,
+        cookies: req.cookies,
+        index: true,
+    });
+}
+
+export function test(){
+    return "soy la funcion test"
+}
